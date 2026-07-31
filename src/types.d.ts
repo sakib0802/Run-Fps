4ff43f04d25dd90e4b8f98568b2d9bcede9b3cba
Sakib <sakib@example.com>
2026-07-31 20:00:46 +0600

    Fix all astro type check errors and add global window types

---
+++ src/types.d.ts
@@ -0,0 +1,7 @@
+interface Window {
+  _rtHeaderInit?: boolean;
+  RunFpsEngine?: {
+    calculateFPS: (config: unknown, game: unknown, gpu: unknown, cpu: unknown) => { avgFps: number };
+    optimizeSettings: (currentFps: number, targetFps: number, game: unknown, result: unknown) => unknown;
+  };
+}
